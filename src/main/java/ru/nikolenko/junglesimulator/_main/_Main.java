69bfe055b94e61eb74b7bb30a339366f7c5702b8
Администратор <gtcgyh@gmail.com>
2023-08-24 22:12:11 +0800

    Create class Main

--- src/main/java/ru/nikolenko/junglesimulator/_main/_Main.java
+++ src/main/java/ru/nikolenko/junglesimulator/_main/_Main.java
@@ -1,4 +1,12 @@
 package ru.nikolenko.junglesimulator._main;
 
+import ru.nikolenko.junglesimulator.entity.Bear;
+import ru.nikolenko.junglesimulator.util.EventSimulator;
+
 public class _Main {
+    public static void main(String[] args) {
+        Bear bear = new Bear();
+        EventSimulator eventSimulator = new EventSimulator();
+        eventSimulator.startSimulation(bear);
+    }
 }
